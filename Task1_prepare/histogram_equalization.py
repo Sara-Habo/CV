07ab651d2005c3ce0229c4659094e3a9c5a6a6b1
Sara-Habo <76527634+Sara-Habo@users.noreply.github.com>
2022-03-16 22:23:32 +0200

    Update histogram_equalization.py

--- Task1_prepare/histogram_equalization.py
+++ Task1_prepare/histogram_equalization.py
@@ -1,5 +1,12 @@
 # -*- coding: utf-8 -*-
 """
+Created on Wed Mar 16 21:25:49 2022
+
+@author: Dell
+"""
+
+# -*- coding: utf-8 -*-
+"""
 Created on Tue Mar 15 19:26:51 2022
 
 @author: Dell
@@ -19,12 +26,12 @@ def Hist(image): #return the image histogram
             h[0,k]=h[0,k]+1
     return h[0]
 
-def Equalize(freq,image_size): #perform histogram equalization on the given frequencies(freq) of the histogtam
+def Equalize(freq,max_intensity,image_size): #perform histogram equalization on the given frequencies(freq) of the histogtam
     pdf=[]
     for x in freq:
         pdf.append(x/image_size)
     cdf = np.cumsum(pdf)
-    new_level=[round(z*255) for z in cdf ]
+    new_level=[round(z*max_intensity) for z in cdf ]
     return new_level
 
 
@@ -39,51 +46,33 @@ def mapping (image,new_level,size): #replace the original value by the new compu
 
 def Histogram_equalize(path,flag): #flag=0 for a grayscale and 1 for rgb
     im=cv2.imread(path,flag)
+    
     if flag==0: #greyscale
-        original_level=Hist(im)
-        new_level=Equalize(original_level,im.shape[0]*im.shape[1])
-        new_image=mapping(im,new_level,[im.shape[0],im.shape[1]])
-   
-    elif flag==1: #RGB image
-        red=im[:,:,2]
-        green=im[:,:,1]
-        blue=im[:,:,0]
-        new_image=im
-       
-        freq_r=Hist(red)
-        freq_g=Hist(green)
-        freq_b=Hist(blue)
+        im=cv2.imread(path,0)
         
-        new_level_r=Equalize(freq_r,im.shape[0]*im.shape[1])
-        new_level_g=Equalize(freq_g,im.shape[0]*im.shape[1])
-        new_level_b=Equalize(freq_b,im.shape[0]*im.shape[1])
-        
-        new_r=mapping(red,new_level_r,[im.shape[0],im.shape[1]])
-        new_image[:,:,2]=new_r
-        
-        new_g=mapping(green,new_level_g,[im.shape[0],im.shape[1]])
-        new_image[:,:,1]=new_g
-        
-        new_b=mapping(blue,new_level_b,[im.shape[0],im.shape[1]])
-        new_image[:,:,0]=new_b
+    elif flag==1:
+        image=cv2.imread(path,1)
+        im_gray=rgb2gray(cv2.cvtColor(image,cv2.COLOR_BGR2GRAY))
+        im=cv2.convertScaleAbs(im_gray)
+    
+    original_level=Hist(im)
+    new_level=Equalize(original_level,np.max(im),im.shape[0]*im.shape[1])
+    new_image=mapping(im,new_level,[im.shape[0],im.shape[1]])
     return new_image
+   
+    
 
 #main
 new_rgb=Histogram_equalize("D:\SBME 3rd year\secomd term\CV\peppers.png",1)
-#cv2.imshow("new_image",new_rgb)
-image_rgb=cv2.imread("D:\SBME 3rd year\secomd term\CV\peppers.png")
-#cv2.imshow("original_rgb",image_rgb)
+rgb_grey=cv2.imread("D:\SBME 3rd year\secomd term\CV\peppers.png")
+im_gray=rgb2gray(cv2.cvtColor(rgb_grey,cv2.COLOR_BGR2GRAY))
+im=cv2.convertScaleAbs(im_gray)
+res_rgb=np.hstack((new_rgb,im))
+cv2.imshow("rgb",res_rgb)
 
-res_rgb = np.hstack((new_rgb,image_rgb))
-# show image input vs output
-cv2.imshow('rgb', res_rgb)
 
 new_grey=Histogram_equalize("D:\SBME 3rd year\secomd term\CV\lena_opencv_gray.jpg",0)
 image_grey=cv2.imread("D:\SBME 3rd year\secomd term\CV\lena_opencv_gray.jpg",0)
 res_grey=np.hstack((new_grey,image_grey))
 cv2.imshow("grey",res_grey)
-
-
-
-
         
