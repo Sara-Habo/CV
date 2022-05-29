ed19a51caa78de253ffa91448b91a425f4b9e3cb
Sara-Habo <sara.habo00@eng-st.cu.edu.eg>
2022-05-29 22:42:02 +0200

    new

--- task2/boundary_trace_2.py
+++ task2/boundary_trace_2.py
@@ -4,21 +4,19 @@ from PIL import Image
 import numpy as np
 
 #--------------------------create image-------------------
-##set back ground
+#set back ground
 black_background = np.zeros((600,600,3),dtype =  "uint8")
-blue_background= cv2.rectangle(black_background, (0, 0), (600, 600),(0, 183, 235), -1)
-print(black_background.shape)
-##(image , start point ,end point ,color ,thikness)  if -1 make this color for whole selected size
-# img1= cv2.rectangle(black_background, (200, 200), (400, 400),(256, 256, 256), -1)
-##set shape
+blue_background= cv2.rectangle(black_background, (0, 0), (600, 600),(0, 183, 235), -1) 
+#set shape
 p1 = (220, 220)
 p2 = (220, 380)
 p3 = (380, 300)
 cv2.line(blue_background, p1, p2, (255, 255, 255), 100)
 cv2.line(blue_background, p2, p3, (255, 255, 255), 100)
 cv2.line(blue_background, p1, p3, (255, 255, 255), 100)
-cv2.rectangle(blue_background, (450, 50), (550, 500),(0,150, 150), -1)
-cv2.rectangle(blue_background, (150, 50), (420, 150),(255,150, 255), -1)
+
+cv2.rectangle(blue_background, (450, 50), (550, 500),(0,150, 150), -1) #blue
+cv2.rectangle(blue_background, (150, 50), (420, 150),(255,150, 255), -1) #red
 
 cv2.imwrite('trace_4.png', blue_background)
 
@@ -39,7 +37,7 @@ def boundary_tracing(image,R_COMPONENT,G_COMPONENT,B_COMPONENT,edge_color):
     #function to change color
     def change_color(row ,col):
         if(edge_color == "RED"):
-            image_detected[row][col][0] = 255
+            image_detected[row][col][0] = 0
             image_detected[row][col][1] = 0
             image_detected[row][col][2] = 0
         elif(edge_color == "YELLOW"):
@@ -70,8 +68,8 @@ def boundary_tracing(image,R_COMPONENT,G_COMPONENT,B_COMPONENT,edge_color):
                 edge_col=j
                 enditerate =1
 
-            if(enditerate ):
-                break
+                if(enditerate ):
+                    break
     ##loop to detect all boundary for shape
     while( stop_condition ):
 
@@ -116,18 +114,14 @@ def boundary_tracing(image,R_COMPONENT,G_COMPONENT,B_COMPONENT,edge_color):
             dir %= 8
 
 
-    #fig, axs = plt.subplots(1,2)
-    #axs[0].imshow(image)#axs[0,0] yields an error
-    #axs[1].imshow(image_detected)
-    #plt.show()
-
-
-image = cv2.imread("C:/Users/NADER/Downloads/trace_4.png")
+image = cv2.imread("trace_4.png")
 image_detected = np.copy(image)
 boundary_tracing(image ,255,255,255, "RED") #for white region
 boundary_tracing(image ,0,150,150 ,"YELLOW") #for green region
 boundary_tracing(image ,255,150, 255, "BLUE")
 fig, axs = plt.subplots(1,2)
 axs[0].imshow(image)#axs[0,0] yields an error
+axs[0].set_title("original image")
 axs[1].imshow(image_detected)
+axs[1].set_title("detected regions")
 plt.show()
